7a28d969bb0b92b7d3f1def234bfc8a4b6ae43df
Pavel Kopylov <pskopylov98@gmail.com>
2021-03-26 12:52:36 +0300

    delete commented code

--- SystemMonitorServer/src/server/Server.cs
+++ SystemMonitorServer/src/server/Server.cs
@@ -9,20 +9,6 @@ namespace SystemMonitorServer.src.server
 
         public static void Start()
         {
-            //var config = new HttpSelfHostConfiguration(GetUrl());
-
-            //config.Formatters.Remove(config.Formatters.XmlFormatter);
-            //config.Routes.MapHttpRoute(
-            //    "DefaultApiGet",
-            //    "monitor/{controller}/{action}",
-            //    new { action = "Get" });
-
-            //using (HttpSelfHostServer server = new HttpSelfHostServer(config))
-            //{
-            //    server.OpenAsync().Wait();
-            //    Console.WriteLine("Press Enter to quit.");
-            //    Console.ReadLine();
-            //}
             using (WebApp.Start<Startup>(url : UrlConfiguration.GetUrl()))
             {
                 Console.WriteLine("Server started");
