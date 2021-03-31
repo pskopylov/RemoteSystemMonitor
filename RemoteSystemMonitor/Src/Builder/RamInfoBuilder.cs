7f785e2574f2c5fae235230302bf4ce700640fc6
pskopylov <pskopylov98@gmail.com>
2021-03-31 17:54:53 +0300

    add ram model

--- RemoteSystemMonitor/Src/Builder/RamInfoBuilder.cs
+++ RemoteSystemMonitor/Src/Builder/RamInfoBuilder.cs
@@ -1,6 +1,9 @@
 ﻿using OpenHardwareMonitor.Hardware;
 using RemoteSystemMonitor.Src.Model;
 using RemoteSystemMonitor.Src.Builder.Sensor;
+using System.Linq;
+using System.Management;
+using System.Collections.Generic;
 
 namespace RemoteSystemMonitor.Src.Builder
 {
@@ -12,11 +15,19 @@ namespace RemoteSystemMonitor.Src.Builder
             var sensors = new RamSensors(ram.Sensors);
             return new RamInfo()
             {
-                Name = ram.Name,
+                Name = GetName(),
                 Load = sensors.Load,
                 UsedMemory = sensors.UsedMemory,
                 AvailableMemory = sensors.AvailableMemory
             };
+        }
+
+        private string GetName()
+        {
+            var searcher = new ManagementObjectSearcher("SELECT PartNumber FROM Win32_PhysicalMemory");
+            return searcher.Get().OfType<ManagementBaseObject>()
+                .Select(item => item.Properties["PartNumber"].Value.ToString())
+                .FirstOrDefault();
         }
 
     }
